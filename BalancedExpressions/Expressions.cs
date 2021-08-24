c9e816c27028a329cb1c593e5153c91cc3353bf9
Dilshod Sadiev <dilshod.sadiev@gmail.com>
2021-08-24 11:59:03 -0400

    Add project files.

---
+++ BalancedExpressions/Expressions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BalancedExpressions
+{
+    class Expressions
+    {
+        public bool isBalanced(string input) {
+            var stack = new Stack<char>();
+            foreach (var ch in input)
+            {
+                if (ch == '(')
+                    stack.Push(ch);
+                if (ch == ')')
+                {
+                    if (stack.Count == 0)
+                        return false;
+                    stack.Pop();
+                }
+                    
+            }
+            return stack.Count==0;
+        }
+    }
+}
---
+++ BalancedExpressions/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BalancedExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "(1+2)";
+            var result = new Expressions().isBalanced(str);
+            Console.WriteLine(result);
+        }
+    }
+}
